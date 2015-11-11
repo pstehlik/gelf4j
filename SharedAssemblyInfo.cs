882dd714fbdaa4085b96973902b75be34daf78fe
Juan J. Chiw <jjchiw@gmail.com>
2015-11-11 13:06:04 +0100

    update nuget package

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("gelf4net")]
-[assembly: AssemblyFileVersion("2.0.3.10")]
-[assembly: AssemblyVersion("2.0.3.10")]
+[assembly: AssemblyFileVersion("2.0.3.11")]
+[assembly: AssemblyVersion("2.0.3.11")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: ComVisible(false)]
 
