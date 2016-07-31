e0fbf75c95d7c632bbfc8611a3c11a2f09f10bda
Juan J. Chiw <jjchiw@gmail.com>
2016-07-31 21:43:20 +0200

    added stopwatch to example project to verify the delay of configuration and sending the gelf if the graylog server is not up

--- examples/SimpleConsoleApplication/Program.cs
+++ examples/SimpleConsoleApplication/Program.cs
@@ -1,32 +1,40 @@
 ﻿using log4net;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleConsoleApplication
 {
-    class Program
+    internal class Program
     {
         protected static readonly ILog log = LogManager.GetLogger(typeof(Program));
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var stop = new Stopwatch();
+            stop.Start();
             log4net.Config.XmlConfigurator.Configure();
+            stop.Stop();
+            Console.WriteLine($"Time elapsed configuration {stop.ElapsedMilliseconds}");
 
             Console.WriteLine("Write a sentence, q to quit");
 
             var text = Console.ReadLine();
 
-            while(text != "q")
+            while (text != "q")
             {
+                stop.Start();
                 log.Debug(String.Format("Randomizer Sentence: {0}", text));
                 log.Debug(new
                 {
                     Message = String.Format("Randomizer Sentence: {0}", text),
                     Open = DateTime.UtcNow
                 });
+                stop.Stop();
+                Console.WriteLine($"Time elapsed configuration {stop.ElapsedMilliseconds}");
                 Console.WriteLine("Sent {0}", text);
 
                 text = Console.ReadLine();
