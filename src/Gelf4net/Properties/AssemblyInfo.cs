a1e7df0e853f9698e73b51322b652dee02a38f73
jjchiw <jjchiw@gmail.com>
2017-05-10 11:21:19 +0200

    Update log4net v2.0.8 and updating version of packages

--- src/Gelf4net/Properties/AssemblyInfo.cs
+++ src/Gelf4net/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("Gelf4Net")]
 [assembly: AssemblyProduct("Gelf4Net")]
-[assembly: AssemblyVersion("3.0.0.5")]
-[assembly: AssemblyFileVersion("3.0.0.5")]
-[assembly: AssemblyInformationalVersion("3.0.0.5")]
+[assembly: AssemblyVersion("3.0.0.6")]
+[assembly: AssemblyFileVersion("3.0.0.6")]
+[assembly: AssemblyInformationalVersion("3.0.0.6")]
 [assembly: AssemblyCopyright("MIT 2017")]
 
