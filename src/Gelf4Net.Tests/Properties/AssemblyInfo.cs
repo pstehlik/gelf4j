44dbbdbcef4b8a025173d65b7088931f1c051083
Will Speak <will.speak@crispthinking.com>
2018-08-10 10:20:29 +0100

    Remove Old Source Directory
    Replace `src/` with re-built directory.

--- src/Gelf4Net.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Gelf4Net")]
-[assembly: AssemblyProduct("Gelf4Net")]
-[assembly: Guid("C92B1448-EF37-432B-B9C2-C843831B3CFA")]
